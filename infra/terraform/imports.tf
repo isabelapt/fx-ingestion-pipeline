8c238d241be01b1c34ebe79a9dd1685660e19adf
Isabela Trindade <isabelatrindade94@hotmail.com>
2026-08-02 22:32:55 -0300

    fix: use static dev IDs in imports.tf to allow clean init and import of existing resources

--- infra/terraform/imports.tf
+++ infra/terraform/imports.tf
@@ -1,45 +1,39 @@
-# All resources are already declared in main.tf and storage.tf.
-# Import blocks are commented out because they are not allowed to contain 
-# variables or local values during the "terraform init" phase.
-# If you need to re-import these resources, uncomment them and temporarily
-# hardcode the IDs.
+import {
+  to = aws_iam_role.lambda_exec_role
+  id = "fx-ingestion-lambda-role-dev"
+}
 
-# import {
-#   to = aws_iam_role.lambda_exec_role
-#   id = local.import_lambda_role_id
-# }
-# 
-# import {
-#   to = aws_iam_policy.lambda_s3_policy
-#   id = local.import_lambda_s3_policy_id
-# }
-# 
-# import {
-#   to = aws_glue_catalog_database.fx_database
-#   id = local.import_glue_database_id
-# }
-# 
-# import {
-#   to = aws_cloudwatch_event_rule.daily_trigger
-#   id = local.import_daily_trigger_id
-# }
-# 
-# import {
-#   to = aws_sns_topic.data_team_alerts
-#   id = local.import_data_team_alerts_id
-# }
-# 
-# import {
-#   to = aws_s3_bucket_notification.bucket_notification
-#   id = local.import_bucket_notification_id
-# }
-# 
-# import {
-#   to = aws_sns_topic.data_ready_alerts
-#   id = local.import_data_ready_alerts_id
-# }
-# 
-# import {
-#   to = aws_cloudwatch_event_rule.s3_raw_upload_rule
-#   id = local.import_s3_raw_upload_rule_id
-# }
+import {
+  to = aws_iam_policy.lambda_s3_policy
+  id = "arn:aws:iam::230850604130:policy/fx-lambda-s3-write-dev"
+}
+
+import {
+  to = aws_glue_catalog_database.fx_database
+  id = "230850604130:fx_rates_db_dev"
+}
+
+import {
+  to = aws_cloudwatch_event_rule.daily_trigger
+  id = "fx-ingestion-daily-cron-dev"
+}
+
+import {
+  to = aws_sns_topic.data_team_alerts
+  id = "arn:aws:sns:us-east-1:230850604130:fx-ingestion-alerts-topic-dev"
+}
+
+import {
+  to = aws_s3_bucket_notification.bucket_notification
+  id = "fx-ingestion-raw-data-dev"
+}
+
+import {
+  to = aws_sns_topic.data_ready_alerts
+  id = "arn:aws:sns:us-east-1:230850604130:fx-ingestion-data-ready-topic-dev"
+}
+
+import {
+  to = aws_cloudwatch_event_rule.s3_raw_upload_rule
+  id = "fx-s3-raw-upload-rule-dev"
+}
