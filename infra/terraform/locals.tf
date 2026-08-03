8b6ea44caa7eb6b88688feef0ba4208fd828bb6b
Isabela Trindade <isabelatrindade94@hotmail.com>
2026-08-02 22:19:29 -0300

    style: run terraform fmt to format locals.tf

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -15,12 +15,12 @@ locals {
   lambda_error_alarm_name         = "fx-ingestor-error-alarm-${var.environment}"
 
   # Import block static ID values
-  import_lambda_role_id      = "fx-ingestion-lambda-role-${var.environment}"
-  import_lambda_s3_policy_id = "arn:aws:iam::230850604130:policy/fx-lambda-s3-write-${var.environment}"
-  import_glue_database_id    = "230850604130:fx_rates_db_${var.environment}"
-  import_daily_trigger_id    = "fx-ingestion-daily-cron-${var.environment}"
-  import_data_team_alerts_id = "arn:aws:sns:us-east-1:230850604130:fx-ingestion-alerts-topic-${var.environment}"
+  import_lambda_role_id         = "fx-ingestion-lambda-role-${var.environment}"
+  import_lambda_s3_policy_id    = "arn:aws:iam::230850604130:policy/fx-lambda-s3-write-${var.environment}"
+  import_glue_database_id       = "230850604130:fx_rates_db_${var.environment}"
+  import_daily_trigger_id       = "fx-ingestion-daily-cron-${var.environment}"
+  import_data_team_alerts_id    = "arn:aws:sns:us-east-1:230850604130:fx-ingestion-alerts-topic-${var.environment}"
   import_bucket_notification_id = "fx-ingestion-raw-data-${var.environment}"
-  import_data_ready_alerts_id = "arn:aws:sns:us-east-1:230850604130:fx-ingestion-data-ready-topic-${var.environment}"
-  import_s3_raw_upload_rule_id = "fx-s3-raw-upload-rule-${var.environment}"
+  import_data_ready_alerts_id   = "arn:aws:sns:us-east-1:230850604130:fx-ingestion-data-ready-topic-${var.environment}"
+  import_s3_raw_upload_rule_id  = "fx-s3-raw-upload-rule-${var.environment}"
 }
